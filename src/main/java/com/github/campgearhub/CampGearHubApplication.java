b65cfdf44b738f93cad3ed90bcd7062d9c9f049c
WooBiju <yunji0409@naver.com>
2025-02-06 15:25:52 +0900

    Initial commit

---
+++ src/main/java/com/github/campgearhub/CampGearHubApplication.java
@@ -0,0 +1,13 @@
+package com.github.campgearhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampGearHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampGearHubApplication.class, args);
+    }
+
+}
